61a32096e2d6724aaaf9b5ea320124824a57c8a5
unknown <khean1014330@my.yosemite.edu>
2025-11-17 22:24:22 -0800

    feat: Implement Ticket (public struct) and Show (private struct)

---
+++ Show.cpp
@@ -0,0 +1,34 @@
+//
+// YOur comments here...
+//
+
+#include "Show.h"
+
+// TODO: Implement the 6 methods (3 getters, 3 setters) for the Show struct.
+//
+// Remember to use the scope resolution operator (Show::) for each method.
+//
+// --- Accessors (Getters) ---
+// (e.g., std::string Show::getTitle() { ... })
+// --- Accessors (Getters) ---
+std::string Show::getTitle() {
+    return title;
+}
+std::string Show::getVenue() {
+    return venue;
+}
+int Show::getShowTime() {
+    return showTime;
+}
+//
+// --- Mutators (Setters) ---
+void Show::setTitle(std::string newTitle){
+    title = newTitle;
+}
+void Show::setVenue(std::string newVenue) {
+    venue = newVenue;
+}
+void Show::setShowTime(int newShowTime) {
+    showTime = newShowTime;
+}
+//
---
+++ Show.h
@@ -0,0 +1,39 @@
+//
+// YOur comments here...
+//
+
+#ifndef FIRSTSTRUCT_SHOW_H
+#define FIRSTSTRUCT_SHOW_H
+
+#include <string>
+
+/**
+ * @struct Show
+ * @brief Holds data for a show, enforcing data hiding.
+ *
+ * This struct uses private members and public accessor/mutator
+ * methods to control how data is accessed and modified.
+ * The method implementations will be in Show.cpp.
+ */
+struct Show {
+private:
+    // TODO: Add the private data members here per Part 2b.
+    std::string title;
+    std::string venue;
+    int showTime;
+
+
+public:
+    // TODO: Add the *declarations* for the 6 public methods here (Part 2b).
+    //
+    // --- Accessors (Getters) ---
+    std::string getTitle();
+    std::string getVenue();
+    int getShowTime();
+    // --- Mutators (Setters) ---
+    void setTitle(std::string newTitle);
+    void setVenue(std::string newVenue);
+    void setShowTime(int newShowTime);
+};
+
+#endif //FIRSTSTRUCT_SHOW_H
